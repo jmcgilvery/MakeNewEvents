9a9d9a38639ae86fcf687940418c181446e64b68
Jordan McGilvery <jordan.mcgilvery@duke.edu>
2020-11-16 10:53:01 -0500

    Initial Commit

---
+++ MakeNew EventsTests/MakeNew_EventsTests.swift
@@ -0,0 +1,34 @@
+//
+//  MakeNew_EventsTests.swift
+//  MakeNew EventsTests
+//
+//  Created by Jordan McGilvery on 11/16/20.
+//  Copyright © 2020 Jordan McGilvery. All rights reserved.
+//
+
+import XCTest
+@testable import MakeNew_Events
+
+class MakeNew_EventsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
